9fe233295bad31b9d03615b4df2f0958fc648e26
ishwaribhade <ishwarimbhade@gmail.com>
2017-03-25 21:15:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,6 +20,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+function hash(input,salt) {
+    var hashed  = crypto.pbkdf2Sync(input, salt, 100000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input',function(req,res){
+    var hashedString=hash(req.params.input,'this-is-a-random-salt');
+    res.send(hashedString);
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -54,15 +64,7 @@ app.get('/test-db',function(req,res){
    }) ;
 });
 
-function hash(input,salt) {
-    var hashed  = crypto.pbkdf2Sync(input, salt, 100000, 512, 'sha512');
-    return hashed.toString('hex');
-}
 
-app.get('/hash/:input',function(req,res){
-    var hashedString=hash(req.params.input,'this-is-a-random-salt');
-    res.send(hashedString);
-});
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
