3ec7b4885d1a03b293381b82c35f8f946f967ea4
ishwaribhade <ishwarimbhade@gmail.com>
2017-03-25 20:35:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -31,6 +31,9 @@ app.get('/article-one',function(req,res) {
     res.send('Article one requested here');
 });
 
+app.get('ui/main.js',function(req,res){
+     res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 var counter=0;
 app.get('/counter',function(req,res){
     counter=counter+1;
