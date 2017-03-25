477e1ac207cdbb7cf21a166c9dcf3a11e28303d1
ishwaribhade <ishwarimbhade@gmail.com>
2017-03-25 20:45:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get('/counter',function(req,res){
 });
 
 var pool=new Pool(config);
-app.get('/testdb',function(req,res){
+app.get('/test-db',function(req,res){
    pool.query('SELECT * FROM testdb',function(err,result){
       if(err)
       {
