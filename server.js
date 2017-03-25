3df69cbaf85620c175dfa0af7889545d3504d2b9
ishwaribhade <ishwarimbhade@gmail.com>
2017-03-25 21:01:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
-
+var crypto=require('crypto');
 var config={
     username:'ishwaribhade',
     database:'ishwaribhade',
@@ -49,10 +49,19 @@ app.get('/test-db',function(req,res){
       }
       else{
           res.send(JSON.stringify(result));
+          
       }
    }) ;
 });
 
+function hash(input) {
+    var hashed  = crypto.pbkdf2Sync('secret', 'salt', 100000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+app.get('/hash/input',function(req,res){
+    var hashedstring=hash(req.params.input);
+    res.send(hashedstring);
+});
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
