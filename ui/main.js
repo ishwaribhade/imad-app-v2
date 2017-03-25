d982ee4c7b017a0d1970aff9a96ff0e05d56d409
ishwaribhade <ishwarimbhade@gmail.com>
2017-03-25 20:06:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 app.get('/article-one',function(req,res) {
     res.send('Article one requested here');
 });
-
+var counter=0;
 app.get('/counter',function(req,res){
     counter=counter+1;
     res.send(counter.toString());
